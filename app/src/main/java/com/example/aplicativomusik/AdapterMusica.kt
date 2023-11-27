485f19f3612e774233f6dec49960d4f66601fd78
SantosKarolayne <karolayne.alves517@gmail.com>
2023-11-27 12:31:15 -0300

    alterei a descrição do feed , e implementei a classe adapter

--- app/src/main/java/com/example/aplicativomusik/AdapterMusica.kt
+++ app/src/main/java/com/example/aplicativomusik/AdapterMusica.kt
@@ -14,18 +14,20 @@ import com.example.aplicativomusik.ConteudoRepositorio
 import com.example.aplicativomusik.R
 import com.example.aplicativomusik.Repositorio
 
-class AdapterMusica (private val context: Context, private val musicaList:List<Musica>): BaseAdapter(){
-    override fun getCount():Int{
+class AdapterMusica(private val context: Context, private val musicaList: List<Musica>) : BaseAdapter() {
+    override fun getCount(): Int {
         return musicaList.size
     }
-    override fun getItem(position:Int):Any{
+
+    override fun getItem(position: Int): Any {
         return musicaList[position]
     }
-    override fun getItemId(position: Int):Long{
+
+    override fun getItemId(position: Int): Long {
         return position.toLong()
     }
-    override fun getView(position:Int,convertView:View?, parent : ViewGroup?):View{
 
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val view: View
         val viewHolder: ViewHolder
 
@@ -42,35 +44,32 @@ class AdapterMusica (private val context: Context, private val musicaList:List<M
         viewHolder.imagem.setImageResource(musica.imagem)
         viewHolder.descricao.text = musica.descricao
 
-
         view.setOnClickListener {
             val intent = Intent(context, Repositorio::class.java)
-            intent.putExtra("descricao", musica.descricao)
+
+            // Aqui você pode adicionar uma lógica para verificar o usuário clicado e abrir o repositório correspondente
+            when (musica.descricao) {
+                "Karolayne@gmail.com              @Karolayne Vejam essa minha nova composição Amor" -> intent.putExtra("repositorio", "RepositorioKarolayne")
+                "Hugo@gmail.com              @Hugo   Da uma olhadinha no meu repositorio , Odio embreve em todas as plataformas digitais" -> intent.putExtra("repositorio", "RepositorioGuilherme")
+                "André@gmail.com              @André   Comprei novas demo amor e odio" -> intent.putExtra("repositorio", "RepositorioHugo")
+                "Guilherme@gmail.com              @Guilherme   Fiz o remix de odio" -> intent.putExtra("repositorio", "RepositorioFabricio")
+                "Fabricio@gmail.com              @Fabricio   Alguém poderia me ensinar a tocar essas melodias" -> intent.putExtra("repositorio", "RepositorioAndre")
+                // Adicione mais casos conforme necessário
+
+                else -> {
+                    // Lógica padrão ou mensagem de erro
+                    intent.putExtra("repositorio", "RepositorioPadrao")
+                }
+            }
+
             context.startActivity(intent)
         }
 
         return view
-
-        if(convertView == null ){
-            view = LayoutInflater.from(context).inflate(R.layout.list_item,parent,false)
-            viewHolder = ViewHolder (view)
-            view.tag = viewHolder
-        }else{
-            view = convertView
-            viewHolder = view.tag as ViewHolder
-        }
-
-        //val musica estava aqui
-        viewHolder.imagem.setImageResource(musica.imagem)
-        viewHolder.descricao.text = musica.descricao
-
-        return view
-    }
-    private class ViewHolder(view:View){
-        val  imagem: ImageView = view.findViewById(R.id.imageRepositorio)
-        val descricao : TextView = view.findViewById(R.id.tvDescricao)
     }
 
-
+    private class ViewHolder(view: View) {
+        val imagem: ImageView = view.findViewById(R.id.imageRepositorio)
+        val descricao: TextView = view.findViewById(R.id.tvDescricao)
+    }
 }
-
--- app/src/main/java/com/example/aplicativomusik/Musica.kt
+++ app/src/main/java/com/example/aplicativomusik/Musica.kt
@@ -11,11 +11,11 @@ data class Musica (
 object RepositorioRepository{
     fun getRepositorio():List<Musica>{
         return listOf(
-            Musica(R.drawable.karol, "Karolayne@gmail.com              @Karolayne   Hoje tirei uma foto"),
-            Musica(R.drawable.hugo, "Hugo@gmail.com              @Hugo   Hoje tirei uma foto"),
-            Musica(R.drawable.andre, "\"André@gmail.com              @André   Hoje tirei uma foto"),
-            Musica(R.drawable.guilherme, "\"Guilherme@gmail.com              @Guilherme   Hoje tirei uma foto"),
-            Musica(R.drawable.fabricio, "\"Fabricio@gmail.com              @Fabricio   Hoje tirei uma foto")
+            Musica(R.drawable.karol, "Karolayne@gmail.com              @Karolayne Vejam essa minha nova composição Amor"),
+            Musica(R.drawable.hugo, "Hugo@gmail.com              @Hugo   Da uma olhadinha no meu repositorio , Odio embreve em todas as plataformas digitais"),
+            Musica(R.drawable.andre, "\"André@gmail.com              @André   Comprei novas demo amor e odio"),
+            Musica(R.drawable.guilherme, "\"Guilherme@gmail.com              @Guilherme   Fiz o remix de odio "),
+            Musica(R.drawable.fabricio, "\"Fabricio@gmail.com              @Fabricio   Alguém poderia me ensinar a tocar essas melodias")
 
         )
 
